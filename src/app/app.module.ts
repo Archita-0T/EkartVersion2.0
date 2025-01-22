bbb71918cc3a78d043c53f236b9ddfb92e462b4c
unknown <ATiwari@quark.com>
2025-01-22 15:23:37 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { ContainerComponent } from './container/container.component';
+import { SearchComponent } from './container/search/search.component';
+import { ProductListComponent } from './container/product-list/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, HeaderComponent, TopHeaderComponent, MainMenuComponent, TopMenuComponent, ContainerComponent, SearchComponent, ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
